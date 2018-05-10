3b7ce5055a460d32cadc2dfbc34f7ee2cfa4dbbc
thunguy@me.com <thunguy@me.com>
2018-05-10 07:09:07 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,53 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles",
+  ]
+  
+  function addElementToBeginningOfArray(array, element) {
+    return [element, ...array]
+  }
+  
+   function addElementToEndOfArray(array, element) {
+    return [...array, element]
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(array, element) {
+    return [element, 1]
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(array, element) {
+    array.unshift(element); {
+      return array
+    }
+  }
+  
+  function destructivelyAddElementToEndOfArray(array, element) {
+    return [1, element]
+  }
+  
+  function destructivelyAddElementToEndOfArray(array, element) {
+    array.push(element); {
+      return array
+    }
+  }
+  
+  function accessElementInArray (array, index) {
+    return array[index]
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(array) {
+    return array.shift();
+  }
+  
+  
+  
+
+  
+  
+  
+  
+
+  
+  
